588ae5952d14a849a39220f66cb72c12cb33e541
Wenchao Li <Lemonz@outlook.com>
2014-11-12 22:41:50 +0800

    Assignement3 of LiWenchao and WangXinyi. 11.12 	new file:   data/text.json 	new file:   index.css 	new file:   index.html 	new file:   js/d3.js 	new file:   js/text.js 	modified:   readme.md 	new file:   screenshot/Image_1_liwenchao_A3.jpg

---
+++ js/text.js
@@ -0,0 +1,121 @@
+//Width and height
+var w = 600;
+var h = 500;
+
+var format = d3.format(",d");
+var color = d3.scale.category10();
+var scale = d3.scale.linear();
+
+//Create SVG element
+var svg = d3.select("body")
+			.append("svg")
+			.attr("width", w)
+			.attr("height", h)
+            .attr("class", "bubbles");
+
+//Initialize a default pack layout for bubbles
+var bubble = d3.layout.pack()
+					  .sort(null)
+					  .size([w, h])
+					  .padding(4);
+ 
+//Global root
+var root;
+
+d3.json("data/text.json", function(error, json) {
+	if (error) {
+		console.log('loading JSON fail!', error);
+	} else {
+		root = json;
+
+        var allRadius = [];
+		//Create nodes group
+		var nodes = svg.selectAll(".nodes")
+		               .data(bubble.nodes(classes(root))
+                       .filter(function(d) { return !d.children; }))
+                       .enter()
+                       .append("g")
+                       .attr("class", "nodes")
+                       .attr("transform", function(d) {
+                            allRadius.push(d.r);
+                            return "translate(" + d.x + "," + d.y + ")"; })
+                       .on("mouseover", mouseover)
+                       .on("mouseout", mouseout)
+                       .on("click",click);
+
+        //Map the radius to opacity[0.4, 0.9] 
+        scale.domain([d3.min(allRadius), d3.max(allRadius)])
+             .range([0.4, 0.9]);
+
+        //Hover to see title
+        nodes.append("title")
+            .text(function(d) { return d.className + ": " + format(d.value); });
+
+		//Create nodes as circles
+		nodes.append("circle")
+			.attr("r", function(d) { return d.r; })
+            .attr("class", "circles")
+            .style("fill", function(d) { return color(d.packageName); })
+            .style("opacity", function(d) { return scale(d.r); });
+
+        //Text for className
+		nodes.append("text")
+			.attr("class", "text")
+			.attr("dy", ".3em")
+			.style("text-anchor", "middle")
+            .style("font-size", function(d) { return (d.r) / 2.2 + "px"; })
+            .style("opacity", function(d) { return scale(d.r); })
+			.text(function(d) { return d.className; });
+	}
+});
+
+// Returns a flattened hierarchy containing all leaf nodes under the root.
+function classes(root) {
+    var classes = [];
+
+    function recurse(name, node) {
+        if (node.children) {
+            node.children.forEach(function(child) {
+                recurse(node.name, child);
+            });
+        } else {
+            classes.push({packageName: name, className: node.name, value: node.count});
+        }
+    }
+
+    recurse(null, root);
+    return {children: classes};
+}
+
+//Click to show the description
+function click(d) {
+    document.getElementById("description").innerHTML = "The word \"<span>" + d.className + "</span>\" is mentioned <span>" + format(d.value) + "</span> times.";
+}
+
+//Emphasize the node when move over
+function mouseover(d) {
+    d3.select(this).select("circle")
+        .transition()
+        .duration(200)
+        .style("opacity", "1");
+
+	d3.select(this).select("text")
+        .transition()
+        .duration(200)
+        .style("opacity", "1")
+        .style("font-weight", "bold");
+}
+
+//Get back to original status
+function mouseout(d) {
+    d3.select(this).select("circle")
+        .transition()
+        .duration(300)
+        .style("opacity", function(d) { return scale(d.r); });
+
+	d3.select(this).select("text") 
+		.transition()
+		.duration(300)
+        .style("opacity", function(d) { return scale(d.r); })
+		.style("font-weight", "normal");
+}
--- readme.md
+++ readme.md
@@ -1,23 +1,25 @@
-#下面介绍github的使用方法：
-###1. github地址：[Assignment3](https://github.com/vis2014/Assignment3).
+#Assignment III: Text Visulization of the Speech "I Have a Dream"
+###1. Github Address：[LiWenchao_WangXinyi_A3](https://github.com/vis2014/Assignment3/tree/LiWenchao_WangXinyi_A3 "LiWenchao & WangXinyi Assignment3").
 
-###2. 安装git：
-+ git工具用来获取远程代码以及提交代码。
-+ 下载地址： [git](http://git-scm.com/downloads) 。
+###2. Package Description
+It includes:
 
-###3. 获取github上的代码：
-+ 在一个单独的文件夹中，例如E:\git，按住shift同时右键，选择在此处打开命令窗口。依次输入以下命令
-+ git clone https://github.com/vis2014/Assignment3.git		//克隆代码,username是vis2014@163.com, password是vis_2014
-+ cd Assignment3		//进入文件夹Assignment3
-+ git checkout –b *local_name* origin/master	//创建自己的分支，*注意*：local_name替换为自己的名字，格式为LastnameFirstname_LastnameFirstname_A3 
++ index.html: The main html to embed D3 script.
++ index.css: CSS of the network visualization and the main page.
++ js/d3.js: D3 library.
++ js/text.js: The D3 script to read text data and create visualization.
++ data/text.json: frequency of different words in the speech.
++ screenshot: One screenshot of the visualization.
 
-###4. 在Assignment3文件夹中放入自己的完整的代码，包括引用的javascript库等
+###3. Operation of the Network Visualization 
++ Point to the circle to get a clearer view.
++ Click a circle, then the relevant data will be displayed above.
 
-###5. 修改readme.md文件，在该文件里写作业的介绍，编写该文件使用的是markdown语法，可以上网上查语法格式，也可以参考这篇文章[markdown语法说明](http://wowubuntu.com/markdown/basic.html)
+###4. Usage and Analysis
++ In the visualization above, each circle represents a frequent word in the speech. The sizes and the opacity of the circles are initially set relating to the frequency of the word in the center. The larger of the circles, the higher frequency of the word. Additionally, the radius is mapped to opacity between 0.4 and 0.9 .
++ After clicking a node, text description is also provided.
++ Accordding to the visualization, the keywords can be inferred from the highlighted word. Such as "freedom", "Negro", "dream" and "justice".
 
-###6. 在以上步骤完成之后可以上传代码，按住shift同时右键，选择在此处打开命令窗口。依次输入以下命令
-+ git add . 	//添加文件
-+ git commit -m "注释" //提交到本地
-+ git push origin *local_name*	//*注意*：和第三步一样，local_name替换为自己的名字，格式为LastnameFirstname_LastnameFirstname_A3，要和上面的local_name保持一致,username是vis2014@163.com, password是vis_2014
-
-###7. 这时再次查看github的代码，[Assignment3](https://github.com/vis2014/Assignment3)，在自己的分支里就可以看到自己的代码了
+###5. Data
++ Data type: JSON.
++ [The Speech "I Have a dream"](http://news.bbc.co.uk/2/hi/americas/3170387.stm "I Have a dream"): The word's frequency is counted by another script language. Some of the words are deleted such as the preposition. Finally it is transformed to the file "text.json".
