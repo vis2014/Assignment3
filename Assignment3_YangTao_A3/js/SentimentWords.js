35f7d9a4a3b2e6d9fc7122b54dd66cf135e1ce9b
youname <you email>
2014-11-18 03:57:59 +0800

    注释

---
+++ Assignment3_YangTao_A3/js/SentimentWords.js
@@ -0,0 +1,181 @@
+﻿// JavaScript source code
+var width = 1024;
+var height = 1000;
+var words = []; //存储情感词
+var wordsvalue = [];//存储极值
+var wordsSelect = []; //存储选择的情感词
+var wordsvalueSelect = [] //存储选择的极值 
+var x = 0, y = 0;
+var size = [30, 50,70];//表示每层放置几个路径
+var fill = d3.scale.category20();//设置20种颜色
+var dataset; //数据源
+var name;//定义标题
+var name_length;//标题的长度
+var circle_r;//圆的半径
+var layer = 3;//表示生成几层的图形
+d3.json("json/SentimentWords.json", function (error, data) { //添加json数据
+   
+    dataset = data;
+});
+    
+    function findWords(value) { //获取情感词
+        wordsSelect = [];
+        x = 0;
+        for (var i = 1; i < dataset.nodes.length; i++){
+            if(value==0){
+                wordsSelect[x] = dataset.nodes[i].SentimentWords;
+                x++;
+             }
+            if(value==1){
+                if (parseFloat(dataset.nodes[i].extremum) > 0) {
+                    wordsSelect[x] = dataset.nodes[i].SentimentWords;
+                    x++;
+                }
+            }
+            if (value == 2) {
+                if (parseFloat(dataset.nodes[i].extremum) < 0) {
+                    wordsSelect[x] = dataset.nodes[i].SentimentWords;
+                    x++;
+                }
+            }
+
+        }
+        return wordsSelect;
+    }
+
+    function findextremum(value) { //获取极值
+        wordsvalueSelect = [];
+        y = 0;
+        for (var i = 1; i < dataset.nodes.length; i++) {
+            if (value == 0) {
+                wordsvalueSelect[y] = parseFloat(dataset.nodes[i].extremum);
+                y++
+            }
+            if (value == 1) {
+                if (parseFloat(dataset.nodes[i].extremum) > 0) {
+                    wordsvalueSelect[y] = parseFloat(dataset.nodes[i].extremum);
+                    y++;
+                }
+            }
+            if (value == 2) {
+                if (parseFloat(dataset.nodes[i].extremum) < 0) {
+                    wordsvalueSelect[y] = parseFloat(dataset.nodes[i].extremum);
+                    y++;
+                }
+            }
+        }
+        return wordsvalueSelect;
+    }
+
+    function setLayerAndSize() { //设置图形层数
+        var word_size = words.length;
+        if (word_size < size[0] + size[1]+size[2]) {
+            if (word_size <= size[0]) {
+                layer = 1;
+                size[2] = 0;
+                size[1] = 0;
+                size[0] = word_size;
+            }
+            if (word_size < size[0] + size[1]){
+                layer = 2;
+                size[2] = 0;
+                size[1] = word_size - size[0];
+                size[0] = 30;
+            }
+            
+        }
+        else {
+            layer = 3;
+            size[0] = 30;
+            size[1] = 50;
+            size[2] = 70;
+        }
+    }
+
+    function createWordCloud() {
+        setLayerAndSize();
+        // 先在 svg 中插入一個圆
+        d3.select("svg").remove();
+        svg = d3.select('body').append('svg').attr("width", width).attr("height", height);
+        svg.append('circle').attr({
+            id: "circle",
+            cx: width / 2,
+            cy: height / 2 -80,
+            r: circle_r,
+            stroke: "white",
+            fill: "#219E3E",
+            'stroke-with': 50
+        });
+        svg.append('text').attr({
+            x: width / 2,
+            y: height / 2-80 ,
+            "font-size": 25 * Math.log(name_length),
+            "text-anchor": "middle",
+            fill: "white"
+        }).text(name);
+        var current_word = 0;
+        for (var i = 0; i < layer; i++) {
+            var part_number = size[i];//第i层的路径数目
+            var r_layer_start = circle_r + 10 + 100 * i;
+            var r_layer_end = circle_r + 200 + 100 * i;
+            var sita = Math.PI / 180 *30;
+            for (var j = 0; j < part_number; j++) {
+                var start_x = width / 2 + r_layer_start * Math.cos(j * 2 * Math.PI / part_number + i * sita);
+                var start_y = height / 2-80  + r_layer_start * Math.sin(j * 2 * Math.PI / part_number + i * sita);
+                var end_x = width / 2 + r_layer_end * Math.cos(j * 2 * Math.PI / part_number + i * sita);
+                var end_y = height / 2 -80 + r_layer_end * Math.sin(j * 2 * Math.PI / part_number + i * sita);
+                svg.append("path").attr({
+                    id: current_word,
+                    d: 'M' + start_x + ' ' + start_y + ' ' + 'L' + end_x + ' ' + end_y
+                }).attr("class", "path");
+                svg.append('text').attr({
+                    x: 5,
+                    y: 10,
+                    "font-size": 10 + 10 * Math.abs(wordsvalue[current_word]),
+                    fill: function () { return fill(words[current_word]); },
+                    "font-weight": "bold"
+                }).append('textPath').attr({
+                    'xlink:href': '#' + current_word
+                }).text(words[current_word])
+                //  .on("mouseover", function () {
+                //      highlight(this);
+                //  })
+                //  .on("mouseout", svg.selectAll("text").style("fill", function () {
+                //      return fill(words[current_word]);
+                //  })
+                //      )
+                current_word = current_word + 1;
+            }
+        }
+
+    }  
+ 
+    function refresh(value) { //根据单选按钮，刷新界面
+        words = [];
+        wordsvalue = [];
+        switch (value) {
+            case 'All':
+                words = findWords(0); //生成情感词数组
+                wordsvalue = findextremum(0); //生成极值数组
+                break;
+            case 'positive':
+                words = findWords(1); 
+                wordsvalue = findextremum(1);
+                break;
+            case 'negative':
+                words = findWords(2);
+                wordsvalue = findextremum(2);
+                break;
+            default: break;
+        }
+       
+        name = value;  //中心圆中的关键字
+        name_length = name.length; 
+        circle_r = 60 * Math.log(name_length) ; //中心圆的大小
+       
+        createWordCloud();
+
+       
+    }
+
+ 
--- readme.md
+++ readme.md
@@ -1,24 +1,30 @@
-@@ -0,0 +1,23 @@
-#下面介绍github的使用方法：
-###1. github地址：[Assignment3](https://github.com/vis2014/Assignment3).
+﻿
 
-###2. 安装git：
-+ git工具用来获取远程代码以及提交代码。
-+ 下载地址： [git](http://git-scm.com/downloads) 。
+##**Introduction**：
 
-###3. 获取github上的代码：
-+ 在一个单独的文件夹中，例如E:\git，按住shift同时右键，选择在此处打开命令窗口。依次输入以下命令
-+ git clone https://github.com/vis2014/Assignment3.git		//克隆代码,username是vis2014@163.com, password是vis_2014
-+ cd Assignment3		//进入文件夹Assignment3
-+ git checkout -b *local_name* origin/master	//创建自己的分支，*注意*：local_name替换为自己的名字，格式为LastnameFirstname_LastnameFirstname_A3 
+   Sentiment Words Refers to the word or words with subjective feelings expressed by commentators,to discriminate the preferences of critics to the evaluation object.
+this data is from part of the data of "汉语情感词极值表.txt"..and transform to .json by myself.
+Sentiment Words Extreme reflect some vocabulary words which brought some emotion,and I according to the extreme divid these words to "Positive" and "Negative",and show them arround a circle As shown below graph.
+The size of the word reflects the strength of the emotional word polarity,I will analysis following.
 
-###4. 在Assignment3文件夹中放入自己的完整的代码，包括引用的javascript库等
+###__DataSet__: This data "汉语情感词极值表.txt" is transformed to the file "SentimentWords.json". 
 
-###5. 修改readme.md文件，在该文件里写作业的介绍，编写该文件使用的是markdown语法，可以上网上查语法格式，也可以参考这篇文章[markdown语法说明](http://wowubuntu.com/markdown/basic.html)
 
-###6. 在以上步骤完成之后可以上传代码，按住shift同时右键，选择在此处打开命令窗口。依次输入以下命令
-+ git add . 	//添加文件
-+ git commit -m "注释" //提交到本地
-+ git push origin *local_name*	//*注意*：和第三步一样，local_name替换为自己的名字，格式为LastnameFirstname_LastnameFirstname_A3，要和上面的local_name保持一致,username是vis2014@163.com, password是vis_2014
 
-###7. 这时再次查看github的代码，[Assignment3](https://github.com/vis2014/Assignment3)，在自己的分支里就可以看到自己的代码了
+
+
+###Description： All Sentiment Words I collected,they are distributed around the circle,and  the Words' size are not same.If the extremum is greater than zero, the word size is larger, more polar,"Positive" performance stronger;If the extreme value is less than zero, the word size is larger, more polar, "Negative" performance stronger.
+ 
+
+
+###link
+
+https://github.com/vis2014/Assignment3/tree/Yangtao_A3
+
+
+
+###ratio： 
+
+Assignment2/Assignment3=0.3/0.7.
+
+
