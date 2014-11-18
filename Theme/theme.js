4e93923d5cc52c3f4d51067a7c84ee671c92376f
Zhangyuan_Mazhenzhen <vis2014@163.com>
2014-11-18 12:00:05 +0800

    A3

---
+++ Theme/theme.js
@@ -0,0 +1,111 @@
+/**
+ * Created by yzhang on 14-11-18.
+ */
+var model={};
+var dataset = [];
+
+d3.json("./theme.json",function(json){
+    model.json = json;
+    dataset = model.json.topics.map(function(d){
+        return {text: d.text, size : d.prob * 500, id :d.id, type: 1}
+    });
+    model.font = "黑体";
+    model.charge = -500;
+    model.generate();
+})
+
+var w = 1000, h = 600;
+var fill = d3.scale.category20b();
+model.layout = d3.layout.cloud().size([w, h])
+    .padding(15)
+    .fontSize(function(d) { return d.size; })
+    .on("end", draw);
+model.vis = d3.select("#content").append("svg")
+    .attr("width", w)
+    .attr("height", h)
+    .append("g")
+    .attr("transform", "translate(300,150)");
+var mouseover = function(){
+    var g = d3.select("g");
+    g.selectAll("text")
+        .data(dataset2)
+        .transition()
+        .duration(1000)
+        .text(function(d){
+            return d;
+        });
+}
+
+function Scale(id){
+    model.generate();
+}
+
+model.generate = function (){
+    model.force = d3.layout.force()
+        .nodes(dataset)
+        .size([w/2, h/2])
+        .charge(model.charge);
+
+    model.layout
+        .font(model.font);
+    model.layout.stop().words(dataset).start();
+    model.texts.style("font-family",model.font)
+        .text(function(d) { return d.text; });
+}
+
+model.reforce = function(){
+    model.force.on("tick", function() {
+        model.texts.attr("transform", function(d) {
+            return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })});
+}
+
+
+
+function Font(value){
+    model.font = value;
+    model.generate();
+}
+
+function getTopicWords(id,text){
+    dataset = model.json.tpcwords[id].words.map(function(d){
+        return {text: d.text, size : 20 + d.prob * 500, id :d.id,type:2}
+    });
+    model.charge = -100;
+    model.generate();
+    d3.select("#header h1").text("The Related Words Of Your Selected Topic:  "+text+"");
+}
+
+function getTopics(){
+    dataset = model.json.topics.map(function(d){
+        return {text: d.text, size : d.prob * 500, id :d.id, type: 1}
+    });
+    model.charge = -500;
+    model.generate();
+    d3.select("#header h1").text("Theme Bomb！Click Word！");
+}
+function draw(words) {
+    if(model.texts)
+        model.texts.remove();
+    model.texts =  model.vis.selectAll("text")
+        .data(words).enter().append("text");
+
+    model.texts.style("font-size", function(d) { return d.size + "px"; })
+        .style("fill", function(d, i) { return fill(i); })
+        .attr("text-anchor", "middle")
+        .on("click",function(d){
+            if( d.type == 1)
+                getTopicWords(d.id,d.text)
+            else
+                getTopics();
+        })
+        .call(model.force.drag)
+        .style("opacity", 1e-6)
+        .transition()
+        .duration(1500)
+        .style("opacity", 1)
+
+    model.force.start();
+    model.reforce();
+}
+
--- readme.md
+++ readme.md
@@ -1,7 +1,17 @@
+﻿# Theme Bomb！
+ 
+###1. Link to Assignmen23:
+http://211.147.15.14/UCAS_14_Fall/index.php/Zhangyuan_Mazhenzhen_A3
 
-###1. 小组成员
-+ 耿佳琪
-+ 王青云
+###2. Dataset:
+theme.json:the data set with JSON format
 
-###2. Github Address: 
-[Assignment3](https://github.com/vis2014/Assignment3)
+###3. Package Description:
++  theme.js:the javascript file for the main control logic.
++  theme.html:the file to generate final visualization.
++  style.css:style for the html file.
++  d3.js:d3 library.
++  d3.min.js:d3 library.
+
+###4. Introduction:
+The theme cloud visualization can present different theme in our social life,and we can change the scale and font of the word.When we click one theme,it will show all the words which is related to.First of all,we make up the data set to construct a JSON file.Then we use word cloud to implement the theme cloud.
